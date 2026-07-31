aa03d7d27641c76732107feca7e83ef4526d0cfe
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-07-31 11:01:42 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,15 +3,15 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(char ch:s){
-            if(ch =='('|| ch=='{'|| ch=='['){
+            if (ch=='('|| ch=='{' || ch=='['){
                 st.push(ch);
             }
             else{
                 if(st.empty()) return false;
-                char top =st.top();
+                char top = st.top();
                 st.pop();
                 if((ch==')' && top!='(')||
-                    (ch=='}' && top!='{' )||
+                    (ch=='}' && top!='{')||
                     (ch==']' && top!='['))
                     return false;
             }
