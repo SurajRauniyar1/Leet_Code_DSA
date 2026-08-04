4863bc6b4d8d87a646df7ce94d862ca7dbb5f0d9
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-08-04 16:16:40 +0530

    Time: 0 ms (100%), Space: 11 MB (72.68%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string token= "";
+        vector<string>st;
+        string token="";
         stringstream ss(path);
-        vector<string> st;
-
         while(getline(ss,token,'/')){
-            if(token == "" || token ==".") continue;
-            if(token !=".."){
+            if(token=="" || token==".") continue;
+            if(token!=".."){
                 st.push_back(token);
             }
             else if(!st.empty()){
                 st.pop_back();
             }
         }
-        if(st.empty())
-            return "/";
-            string result = "";
-            for(auto &token:st){
-                result = result+"/"+token;
-            }
-            return result;
+        if(st.empty()) return "/";
+        string ans="";
+        for(auto &token:st){
+            ans +='/'+token;
+      
+        }
+       return ans;
     }
 };
