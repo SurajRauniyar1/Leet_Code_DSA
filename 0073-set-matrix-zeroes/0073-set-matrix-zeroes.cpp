26560580ddaff632d97154b4e31751937dda845b
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-08-16 16:10:18 +0530

    Time: 3 ms (26.43%), Space: 21.2 MB (17.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -8,16 +8,20 @@ public:
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;col[j]=1;}
+                    row[i]=1;
+                    col[j]=1;
+                }
             }
 
         }
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(row[i]==1||col[j]==1){
+                if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
                 }
             }
+
         }
+
     }
 };
