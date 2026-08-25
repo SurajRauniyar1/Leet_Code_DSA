e4be7579b9ee21e60da9ff490fb36fafe4e8ce9d
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-08-25 15:37:54 +0530

    Time: 9 ms (27.68%), Space: 20 MB (39.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,18 +5,20 @@ public:
              vector<vector<int>>& graph,
              vector<int>& state) {
 
-    
+        // Currently visiting → cycle
         if(state[node] == 1) {
             return false;
         }
 
-        
+        // Already completely processed
         if(state[node] == 2) {
             return true;
         }
+
+        // Mark as currently visiting
         state[node] = 1;
 
-  
+        // Visit neighbours
         for(int neighbor : graph[node]) {
 
             if(!dfs(neighbor, graph, state)) {
@@ -24,7 +26,7 @@ public:
             }
         }
 
-       
+        // Completely processed
         state[node] = 2;
 
         return true;
@@ -34,10 +36,10 @@ public:
     bool canFinish(int numCourses,
                    vector<vector<int>>& prerequisites) {
 
-  
+        // Create graph
         vector<vector<int>> graph(numCourses);
 
-    
+        // Build graph
         for(auto p : prerequisites) {
 
             int course = p[0];
@@ -46,9 +48,12 @@ public:
             graph[prerequisite].push_back(course);
         }
 
-   
+        // 0 = not visited
+        // 1 = currently visiting
+        // 2 = completely processed
         vector<int> state(numCourses, 0);
 
+        // Check every course
         for(int i = 0; i < numCourses; i++) {
 
             if(!dfs(i, graph, state)) {
