551bf6583d50744da1bd309c4027d822d91aa832
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-08-27 15:19:27 +0530

    Time: 23 ms (70.44%), Space: 25.9 MB (71.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        seen = set()
+        seen=set()
 
         for num in nums:
             if num in seen:
                 return True
             seen.add(num)
         return False
-        
