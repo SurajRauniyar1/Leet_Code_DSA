a6bdb0892e73185aeb43eb5ec3b73b59486f3682
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-08-31 14:12:35 +0530

    Time: 5 ms (43.23%), Space: 19.3 MB (72.91%) - LeetHub

---
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        sum=0
+        for i,num in enumerate(nums):
+            if i.bit_count()==k:
+                sum+=num
+        return sum
+
+        
