662f7cd7c24314d2fb6afdc011d02fefca7adde0
SurajRauniyar1 <104727672+SurajRauniyar1@users.noreply.github.com>
2026-09-10 11:08:03 +0530

    Time: 15 ms (43.34%), Space: 19.9 MB (22.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,6 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         mp={}
         s=list(s)
+        if len(s)!=len(t): return False
         for ch in t:
             mp[ch]=mp.get(ch,0)+1
         for i in range(len(s)):
@@ -10,5 +11,6 @@ class Solution:
             mp[s[i]]-=1
             if mp[s[i]]<0:
                 return False
+        if any(values!=0 for values in mp.values()): return False
         return True
         
